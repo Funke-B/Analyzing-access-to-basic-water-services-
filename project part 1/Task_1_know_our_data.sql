1d5a48335157555641386ffc1ca642d7af1d57c3
Funke Bolarin <bolarinoluwafunke1@gmail.com>
2024-12-14 13:52:18 +0100

    Create Task_1_know_our_data.sql

---
+++ project part 1/Task_1_know_our_data.sql
@@ -0,0 +1,20 @@
+/*Get to know our data:
+Start by retrieving the first few records from each table. How many tables are there in our database? What are the names of these tables? Once you've identified the tables, write a SELECT statement to retrieve the first five records from each table. As you look at the
+data, take note of the columns and their respective data types in each table. What information does each table contain?*/
+
+SELECT *
+FROM employee
+LIMIT 5;
+SELECT 
+    *
+FROM location    
+LIMIT 10;
+SELECT *
+FROM water_source
+LIMIT 5;
+SELECT *
+FROM water_quality
+LIMIT 5;
+SELECT *
+FROM well_pollution
+LIMIT 5;
